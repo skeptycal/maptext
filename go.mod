2a7942af70ac354d1f4b641fc1ac22a5284735fd
Michael Treanor <skeptycal@gmail.com>
2022-03-03 12:32:53 -0600

    add tests

--- go.mod
+++ go.mod
@@ -1,17 +1,3 @@
 module github.com/skeptycal/maptext
 
 go 1.17
-
-require github.com/skeptycal/testes v0.1.0-1645859773236804
-
-require (
-	github.com/fatih/color v1.13.0 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/skeptycal/errorlogger v0.4.0 // indirect
-	github.com/skeptycal/types v0.5.0 // indirect
-	golang.org/x/sys v0.0.0-20220223155357-96fed51e1446 // indirect
-)
--- maptext_test.go
+++ maptext_test.go
@@ -1,10 +1,9 @@
 package maptext
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
-
-	"github.com/skeptycal/testes"
 )
 
 func TestNewRoot(t *testing.T) {
@@ -15,18 +14,30 @@ func TestNewRoot(t *testing.T) {
 		// parent   *node
 		// left     *node
 		// right    *node
-		want *node
+		want    *node
+		wantErr bool
 	}{
-		// TODO: Add test cases.
-		{"root", "root", []byte("rootData"), &node{"root", []byte("rootData"), nil, nil, nil}},
+		{"root", "bad_name", []byte("failData"), &node{"badName", []byte("failData"), nil, nil, nil}, true},
+		{"root", "root", []byte("rootData"), &node{"root", []byte("rootData"), nil, nil, nil}, true},
+		{"bad data - faker", "fail", []by te("failData"), &node{"fail", []byte("failData"), nil, nil, nil}, true},
+		{"bad parent", "fail", []byte("failData"), &node{"fail", []byte("failData"), nil, nil, nil}, true},
+		{"bad left", "fail", []byte("failData"), &node{"fail", []byte("failData"), nil, nil, nil}, true},
+		{"bad right", "fail", []byte("failData"), &node{"fail", []byte("failData"), nil, nil, nil}, true},
+
+		// False Negative for testing
+		// {"false negative", "correct", []byte("failData"), &node{"correct", []byte("failData"), nil, nil, nil}, false},
+		// False Positive for testing
+		// {"false positive", "incorrect", []byte("rootData"), &node{"root", []byte("rootData"), nil, nil, nil}, true},
 	}
 	for _, tt := range tests {
-
-		testes.TRun()
-		t.Run(tt.testName, func(t *testing.T) {
-			if got := NewRoot(tt.name, tt.data); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewRoot() = %v, want %v", got, tt.want)
-			}
-		})
+		TRun(t, tt.testName, tt.name, NewRoot(tt.name, tt.data), tt.want, tt.wantErr)
 	}
+}
+
+func TRun(t *testing.T, testName, argName string, got, want Any, wantErr bool) {
+	t.Run(fmt.Sprintf("%s(%s)", testName, argName), func(t *testing.T) {
+		if !reflect.DeepEqual(got, want) == !wantErr {
+			t.Errorf("NewRoot() = %v, want %v", got, want)
+		}
+	})
 }
